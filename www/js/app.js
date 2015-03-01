1ba3922df177ba9bd7cde4a9a687132f8391a11c
xbarrufet <xbarrufetm@gmail.com>
2015-03-01 12:21:42 +0100

    v2

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -30,7 +30,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   )
   .state('serveidetail', {
-    url: "/serveis/:serveiId",
+    url: "/serveis/:serveiId/:modus",
     templateUrl: "templates/serveidetail.html",
     controller: 'ServeisCtrl'
       
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,25 @@
 angular.module('starter.controllers', [])
 
+
+.factory('Camera', ['$q', function($q) {
+
+  return {
+    getPicture: function(options) {
+      var q = $q.defer();
+
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, options);
+
+      return q.promise;
+    }
+  }
+}])
+
+
 .controller('AppCtrl', function($scope, $ionicModal, $timeout,$state) {
   // Form data for the login modal
   $scope.loginData = {};
@@ -33,23 +53,106 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('ServeisCtrl', function($scope,$state,$stateParams,$location) {
+.controller('ServeisCtrl', function($scope,$state,$stateParams,$location, Camera ,$cordovaBarcodeScanner, $ionicPopup) {
  
+    function alerta(msg) {
+        var alertPopup = $ionicPopup.alert ({
+            title: 'Task Garden',
+            template: msg
+        });
+        alertPopup.then(function(res) {
+            console.log('Thank you for not eating my delicious ice cream cone');
+        });
+    }
+    
+    
+    $scope.getPicture = function() {
+            Camera.getPicture().then(function(imageURI) {
+              console.log(imageURI);
+            }, function(err) {
+              console.err(err);
+            });
+          };
+    
+    $scope.scanQRCode = function() {
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
+         //chec if exist
+        if( getServei(imageData.text)!=null)
+            $state.go('serveidetail',{"serveiId":imageData.text,"modus":"edit"});
+        else
+            alerta("El codi" + imageData.text + " no es vàlid");
+         console.log("Barcode Format -> " + imageData.format);
+         console.log("Cancelled -> " + imageData.cancelled);
+        }, function(error) {
+            console.log("An error happened -> " + error);
+        });
+    }
+    
+     $scope.scanQRCodeEND = function() {
+         $cordovaBarcodeScanner.scan().then(function(imageData) {
+         //chec if exist
+         if($scope.serveiSelected.codi== imageData.text)
+             $state.go("serveis");
+         else 
+              alerta("El codi" + imageData.text + " no es vàlid");
+         console.log("Barcode Format -> " + imageData.format);
+         console.log("Cancelled -> " + imageData.cancelled);
+        }, function(error) {
+            console.log("An error happened -> " + error);
+        });
+    }
+    
+    
+    
+    $scope.testScan =function(codi) {
+        if( getServei(codi)!=null)
+            $state.go('serveidetail',{"serveiId":codi,"modus":"edit"});
+        else
+            alerta("El codi no es vàlid");
+    }
+    
+    
+     $scope.testScanEND =function(codi) {
+        if($scope.serveiSelected.codi== codi)
+        {
+            $state.go('serveis');
+        }else 
+              alerta("El codi" + codi + " no es vàlid");
+    }
+    
+    $scope.modus=$stateParams.modus;
+   
     $scope.Tasques = [{Descripcio:'Tallar gespa'},{Descripcio:'Podar  arbres'},{Descripcio:'Fumigar'}]
 
-     $scope.go = function ( path ) {
-        $state.go(path); 
-  };
+    $scope.entrarServei=function() {
+        $scope.modus="edit";
+    
+    };
+    
     
    $scope.parametres=$stateParams;
     
+    
+function getServei(codi) {
+    var pos=-1;
+    for (t=0;t<$scope.ServeisActius.length;t++) {
+        if($scope.ServeisActius[t].codi==codi)
+            return $scope.ServeisActius[t];
+    }
+    return null;
+}
+    
+    
     $scope.ServeisActius = [
-    { id: 1, Client: 'Xavier Barrufet', Adressa: 'C/Jaume I 40', DataActiu:'20/11/2014' ,Prioritat:'Alta', Status:'Pendent', NumIncidencies:1,NumTasques:2,Tasques:[{Descripcio:'Tallar gespa',Status:'Pendent',Tipus:'Incidencia'},{Descripcio:'Podar  arbres',Status:'Pendent',Tipus:'Servei'},{Descripcio:'Fumigar',Status:'Pendent',Tipus:'Servei'}]},
-    { id: 2, Client: 'Toni Duran', Adressa: 'C/Verge Montserrat 11', DataActiu:'13/02/2015', Prioritat:'Alta', Status:'Pendent',NumIncidencies:1,NumTasques:2,Tasques:[{Descripcio:'Tallar gespa',Status:'Pendent',Tipus:'Incidencia'},{Descripcio:'Podar  arbres',Status:'Pendent',Tipus:'Servei'},{Descripcio:'Fumigar',Status:'Pendent',Tipus:'Servei'}]},
-    { id: 3, Client: 'Kevin Roldan', Adressa: 'C/Valencia 24', DataActiu:'17/01/2015', Prioritat:'Normal', Status:'Parcial', NumIncidencies:1,NumTasques:2,Tasques:[{Descripcio:'Tallar gespa',Status:'Pendent',Tipus:'Incidencia'},{Descripcio:'Podar  arbres',Status:'Pendent',Tipus:'Servei'},{Descripcio:'Fumigar',Status:'Pendent',Tipus:'Servei'}]}
+    { codi: '112233445566', Client: 'Xavier Barrufet', Adressa: 'C/Jaume I 40', DataActiu:'20/11/2014' ,Prioritat:'Alta', Status:'Pendent', NumIncidencies:1,NumTasques:2,Tasques:[{Descripcio:'Tallar gespa',Status:'Pendent',Tipus:'Incidencia'},{Descripcio:'Podar  arbres',Status:'Pendent',Tipus:'Servei'},{Descripcio:'Fumigar',Status:'Pendent',Tipus:'Servei'}]},
+    { codi: '665544332211', Client: 'Toni Duran', Adressa: 'C/Verge Montserrat 11', DataActiu:'13/02/2015', Prioritat:'Alta', Status:'Pendent',NumIncidencies:1,NumTasques:2,Tasques:[{Descripcio:'Tallar gespa',Status:'Pendent',Tipus:'Incidencia'},{Descripcio:'Podar  arbres',Status:'Pendent',Tipus:'Servei'},{Descripcio:'Fumigar',Status:'Pendent',Tipus:'Servei'}]},
+    { codi: '332211665544', Client: 'Kevin Roldan', Adressa: 'C/Valencia 24', DataActiu:'17/01/2015', Prioritat:'Normal', Status:'Parcial', NumIncidencies:1,NumTasques:2,Tasques:[{Descripcio:'Tallar gespa',Status:'Pendent',Tipus:'Incidencia'},{Descripcio:'Podar  arbres',Status:'Pendent',Tipus:'Servei'},{Descripcio:'Fumigar',Status:'Pendent',Tipus:'Servei'}]}
   ];
-     
-   $scope.serveiSelected =  $scope.ServeisActius[$stateParams.serveiId-1];   
+ 
+ var sselected =    getServei( $stateParams.serveiId);
+ if(sselected!=null)
+     $scope.serveiSelected =  sselected;  
+
  
 })
 
